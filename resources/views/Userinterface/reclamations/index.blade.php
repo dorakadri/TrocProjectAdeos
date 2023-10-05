e494ae70fb5642294d37aca21f2300b3efc786c4
unknown <eya_khechine@hotmail.com>
2023-10-05 09:26:37 +0100

    fix front

--- resources/views/Userinterface/reclamations/index.blade.php
+++ resources/views/Userinterface/reclamations/index.blade.php
@@ -1,46 +1,87 @@
 @extends('components.layout')
 @section('content')
+<style>
+    .card-title {
+        font-size: 20px; /* Increase font size for Title */
+        font-weight: bold; /* Make Title bold */
+    }
 
+    .status {
+        font-size: 20px;
+        font-weight: bold; /* Customize font size for Status */
+    }
+
+    .response {
+        color: #06BFF1; /* Change text color for Responses to blue */
+    }
+
+    /* Add these two classes for red and green status */
+    .status-not-treated {
+        color: red;
+    }
+
+    .status-treated {
+        color: green;
+    }
+
+    /* Add a border to the card */
+ .card {
+    border: 1px solid #06BFF1; /* Set the border color to #06BFF1 */
+    border-radius: 5px; /* Optional: Add rounded corners */
+}
+
+</style>
 <div id="content-page" class="content-page">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12">
-        <div class="card position-relative inner-page-bg bg-primary" style="height: 150px">
-          <div class="inner-page-title">
-            <h3 class="text-white">My reclamations</h3>
-          </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card position-relative inner-page-bg bg-primary" style="height: 150px">
+                    <div class="inner-page-title">
+                        <h3 class="text-white">My reclamations</h3>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="row mt-3">
-      <div class="col-sm-12">
-        <a href="{{ route('reclamation.create') }}" class="btn btn-sm btn-success">+ Add Reclamation</a>
-      </div>
-    </div>
-    <div class="row mt-3">
-      @foreach($reclamations as $reclamation)
-      <div class="col-sm-4 mb-3">
-        <div class="card">
-          <div class="card-body" style="min-height: 150px;"> <!-- Set a minimum height -->
-            <h4 class="card-title">Title: {{ $reclamation->title }}</h4>
-            <h5 class="card-title">Status: {{ $reclamation->status }}</h5>
-            <p class="card-text">Description: {{ $reclamation->description }}</p>
-            @foreach($reclamation->reponses as $reponse)
-            <p class="card-text">Responses: {{ $reponse->description }}</p>
+        <div class="row mt-3">
+            <div class="col-sm-12">
+                <a href="{{ route('reclamation.create') }}" class="btn btn-sm btn-success ">+ Add Reclamation</a>
+            </div>
+        </div>
+        <div class="row mt-3">
+            @foreach($reclamations as $reclamation)
+            <div class="col-sm-4 mb-3">
+                <div class="card">
+                    <div class="card-body" style="min-height: 150px;"> <!-- Set a minimum height -->
+                        <h3 class="card-title"> 
+                            <span class="status 
+                                @if($reclamation->status == 'not_treated') 
+                                    status-not-treated
+                                @else
+                                    status-treated
+                                @endif
+                            ">
+                                {{ $reclamation->status }}
+                            </span>
+                        </h3>
+                        <h4 class="card-status"> {{ $reclamation->title }}</h4>
+
+                        <p class="card-text">{{ $reclamation->description }}</p>
+                        @foreach($reclamation->reponses as $reponse)
+                        <p class="card-text response">Responses: {{ $reponse->description }}</p>
+                        @endforeach
+                    </div>
+                    <div class="card-footer text-center">
+                        <form method="post" action="{{ route('reclamation.destroy', ['reclamation' => $reclamation]) }}">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
             @endforeach
-          </div>
-          <div class="card-footer text-center">
-            <form method="post" action="{{ route('reclamation.destroy', ['reclamation' => $reclamation]) }}">
-              @csrf
-              @method('delete')
-              <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-            </form>
-          </div>
         </div>
-      </div>
-      @endforeach
     </div>
-  </div>
 </div>
 
 @endsection
